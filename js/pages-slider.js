b80d0f9fa79db9738dca2cf5879e6214a39f24c4
holyavo <34104126+holyavo@users.noreply.github.com>
2021-02-26 20:31:23 +0300

    Add files via upload

--- js/pages-slider.js
+++ js/pages-slider.js
@@ -52,3 +52,39 @@ function addingBackground(page, bg){
         bg.classList.remove('bg-active')
     }
 }
+
+swipingPages()
+
+//Swiping pages on phones
+function swipingPages(){
+    let wrap = document.querySelector('.wrapper')
+    let y = null
+    let x = null
+
+    wrap.addEventListener('touchstart', e => {
+        y = e.touches[0].pageY
+        x = e.touches[0].pageX
+    });
+    wrap.addEventListener('touchmove', e => {
+
+        if (!y) return;
+
+        let activePage = findingActiveLink(pages, 'active-page')
+
+        if(e.touches[0].pageY > y){
+            console.log(e.touches[0].pageY > y && ((e.touches[0].pageX + x) < -1000))
+            let previousPage = activePage.parentNode.previousElementSibling?.children[0] || pages[pages.length-1]
+            changePage(null, previousPage)
+        }else{
+
+            let nextPage = activePage.parentNode.nextElementSibling?.children[0] || pages[0]
+            changePage(null, nextPage)
+
+        }
+
+        y = null;
+    });
+}
+
+
+
